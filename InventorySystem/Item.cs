c114ad18e315767eb850a6ca8dc0990f84695cbe
Emanon <155685858+EmanonR@users.noreply.github.com>
2025-02-10 14:34:51 +0100

    Update Item.cs

--- InventorySystem/Item.cs
+++ InventorySystem/Item.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class Item : ScriptableObject
 {
+  public sprite icon;
+  public new string name;
 
+  public bool isKeyItem, stackable;
+
+  public enum onUse {
+    none,
+    consumable,
+    equipable
+  }
+
+  void useItem()
+  {
+  }
 }
