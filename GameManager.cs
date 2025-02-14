77665361669282dd813ddf0921241dfc9cd0296d
Emanon <155685858+EmanonR@users.noreply.github.com>
2025-02-14 16:13:20 +0100

    Create GameManager.cs

---
+++ GameManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] int targetFPS;
+
+    public Transform player;
+    public InventoryManager inventoryManager;
+
+    public bool gamePaused;
+    public GameObject pausePanel;
+
+    public static GameManager instance;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        Application.targetFrameRate = targetFPS;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        inventoryManager = GetComponent<InventoryManager>();
+        pausePanel.SetActive(gamePaused);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gamePaused = !gamePaused;
+        }
+
+        Time.timeScale = gamePaused ? 0f : 1f;
+        pausePanel.SetActive(gamePaused);
+    }
+
+    public void PauseSwitch()
+    {
+        gamePaused = !gamePaused;
+    }
+}
