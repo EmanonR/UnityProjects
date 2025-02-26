af15e83902af513bffca13c28f480ef9da52f110
Emanon <155685858+EmanonR@users.noreply.github.com>
2025-02-26 22:06:58 +0100

    Create BattleTrigger.cs

---
+++ TurnBasedCombat/BattleTrigger.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleTrigger : MonoBehaviour
+{
+    public float triggerRange = 1f;
+    public BattleLayout battle;
+
+    Transform player;
+    bool triggered;
+
+    private void Start()
+    {
+        player = GameManager.instance.player;
+    }
+
+
+    public void Update()
+    {
+        if (player == null) player = GameManager.instance.player;
+        if (triggered) return;
+
+        float distance = Vector3.Distance(player.position, transform.position);
+
+        if (distance <= triggerRange)
+        {
+            print("Start battle!");
+            //Trigger Battle
+            Destroy(gameObject);
+
+            CombatManager.instance.StartBattle(battle);
+            triggered = true;
+
+        }
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+
+        Gizmos.DrawWireSphere(transform.position, triggerRange);
+    }
+}
