3706c90c73b2d44c952ba0cd31d62bb626056909
Emanon <155685858+EmanonR@users.noreply.github.com>
2025-03-01 03:09:50 +0100

    Update and rename EnemyBattleEntity.cs to BattleEntity.cs

---
+++ TurnBasedCombat/BattleEntity.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleEntity : MonoBehaviour
+{
+    public string bio = "No description";
+    public int maxHp = 75, dmg = 5, def = 10, spd = 1;
+
+    public int currentHp, speedCount;
+
+    public Attack[] attacks;
+    public List<StatusEffect> appliedEffects;
+
+    public void TakeDamage(int dmg)
+    {
+        currentHp -= Mathf.CeilToInt(dmg / ((def + 100) / 100));
+    }
+
+    public void UpdateSpeed()
+    {
+        speedCount += spd;
+    }
+
+    public void ResetSpeed()
+    {
+        speedCount = 0;
+    }
+
+    public void AddHP(int hp)
+    {
+        currentHp = Mathf.Clamp(currentHp + hp, 0, maxHp);
+    }
+
+    public void AddEffect(StatusEffect newEffect)
+    {
+        appliedEffects.Add(newEffect);
+    }
+
+    public Attack GetAttack(int i)
+    {
+        return attacks[i];
+    }
+
+    public void Die()
+    {
+
+    }
+}
