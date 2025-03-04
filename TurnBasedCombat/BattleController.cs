c684168b6b75ed60e65baee2d6cddfc57579acc7
Emanon <155685858+EmanonR@users.noreply.github.com>
2025-03-05 00:41:49 +0100

    Update BattleController.cs

--- TurnBasedCombat/BattleController.cs
+++ TurnBasedCombat/BattleController.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public enum BattleState { Start, PlayerTurn, EnemyTurn, Won, Lost, End }
+public enum TurnState { PickingOption, ChooseTarget, End}
 
 public class BattleController : MonoBehaviour
 {
+    #region variables
+
+    [Header("Inputs")]
+    public KeyCode attackButton = KeyCode.Z;
+    public KeyCode cancelButton = KeyCode.X;
+
     [Header("UI")]
     public GameObject attackButtonPrefab;
     public GameObject attackButtonParent;
+    public GameObject targetGraphic;
 
 
     public float enemySpawnRange, playerSpawnRange;
@@ -28,44 +38,70 @@ public class BattleController : MonoBehaviour
     [Header("Battle State")]
     public int currentTurn = 0;
 
+    [HideInInspector] public List<GameObject> attackButtons;
+
     BattleLayout currentBattleLayout;
     BattleState battleState;
-    bool playerTurn;
+    TurnState turnState;
     BattleEntity turnEntity;
-
+    Attack entityChosenAttack;
     Transform cam;
+    bool playerTurn;
+    int targetIndex;
+    #endregion
 
+
+    #region Awake Update
     private void Awake()
     {
         cam = Camera.main.transform;
     }
 
+    private void Update()
+    {
+        MoveCamToPosition();
+
+        if (playerTurn)
+            switch (turnState)
+            {
+                case TurnState.PickingOption:
+                    break;
+                case TurnState.ChooseTarget:
+                    targetGraphic.SetActive(true);
+                    PlayerTargetingLogic();
+                    DisplayTargetGraphic();
+                    break;
+                case TurnState.End:
+                    targetGraphic.SetActive(false);
+                    AttackEntity(playerTurn ? EnemyParty[targetIndex].GetComponent<BattleEntity>() : PlayerParty[targetIndex].GetComponent<BattleEntity>(),
+                        turnEntity, entityChosenAttack);
+                    break;
+                default:
+                    break;
+            }
+    }
+    #endregion
+
+
+    #region setup
     public void SetupBattle(BattleLayout battleLayout)
     {
         battleState = BattleState.Start;
         currentBattleLayout = battleLayout;
+
+        targetGraphic.gameObject.SetActive(false);
 
         SpawnEntities();
 
         StartBattle();
 
     }
-
-    private void Update()
+    private void StartBattle()
     {
-        MoveCamToPosition();
+        currentTurn = 0;
+        StartNextTurn();
     }
 
-    void MoveCamToPosition()
-    {
-        //Move camera to first player in queue
-        cam.position = CalculateSpawnPoint(1, 1, playerSpawnRange);
-        cam.position += transform.right * camOffset.x;
-        cam.position += transform.up * camOffset.y;
-        cam.position += transform.forward * camOffset.z;
-
-        cam.LookAt(camGaze);
-    }
 
     private void SpawnEntities()
     {
@@ -78,6 +114,9 @@ public class BattleController : MonoBehaviour
         {
             GameObject newEnemy = Instantiate(enemy);
             EnemyParty.Add(newEnemy);
+
+            BattleEntity entityData = newEnemy.GetComponent<BattleEntity>();
+            entityData.currentHp = entityData.maxHp;
         }
 
         //Spawn Players
@@ -85,6 +124,9 @@ public class BattleController : MonoBehaviour
         {
             GameObject newPartyMem = Instantiate(partyMem);
             PlayerParty.Add(newPartyMem);
+
+            BattleEntity entityData = newPartyMem.GetComponent<BattleEntity>();
+            entityData.currentHp = entityData.maxHp;
         }
 
         enemyCount = EnemyParty.Count;
@@ -104,14 +146,21 @@ public class BattleController : MonoBehaviour
 
     }
 
-    private void StartBattle()
-    {
-        currentTurn = 0;
-        StartNextTurn();
-    }
+    #endregion
 
+
+    #region Battle handling
     private void StartNextTurn()
     {
+        attackButtonParent.SetActive(false);
+        targetIndex = 0;
+
+        if (battleState == BattleState.Lost || battleState == BattleState.Won)
+        {
+            PostBattle();
+            return;
+        }
+
         currentTurn++;
         // Implement turn logic here
 
@@ -135,7 +184,7 @@ public class BattleController : MonoBehaviour
             if (i != 0)
                 attackInfo += ", ";
 
-                attackInfo += turnEntity.attacks[i].name;
+            attackInfo += turnEntity.attacks[i].name;
         }
 
         print("Current turn is " + turnObj.name);
@@ -151,16 +200,22 @@ public class BattleController : MonoBehaviour
 
     public void PlayerTurn()
     {
-        //Display choices
+        turnState = TurnState.PickingOption;
 
-        //Await input
+        //Display choices
+        InstantiateAttackButtons();
     }
 
     public void EnemyTurn()
     {
+        print("Enemy attacks");
+
         //Select random attack
+        int ran = Random.Range(0, turnEntity.attacks.Length);
+        BattleEntity target = PlayerParty[Random.Range(0, PlayerParty.Count)].GetComponent<BattleEntity>();
 
         //Perform attack on target
+        AttackEntity(target, turnEntity, turnEntity.attacks[ran]);
     }
 
     public void PostBattle()
@@ -168,8 +223,12 @@ public class BattleController : MonoBehaviour
         switch (battleState)
         {
             case BattleState.Won:
+                print("Player has won");
+                EndBattle();
                 break;
             case BattleState.Lost:
+                print("Player has Lost");
+                EndBattle();
                 break;
             default:
                 break;
@@ -179,6 +238,151 @@ public class BattleController : MonoBehaviour
     public void EndBattle()
     {
         CombatManager.instance.EndBattle();
+    }
+
+
+    #endregion
+
+    #region Battle Logic
+    void PlayerTargetingLogic()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            // Left
+            if (targetIndex == 0)
+                targetIndex = EnemyParty.Count - 1;
+
+            else
+                targetIndex--;
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            // Right
+            if (targetIndex == EnemyParty.Count - 1)
+                targetIndex = 0;
+
+            else
+                targetIndex++;
+        }
+
+        if (Input.GetKeyDown(attackButton))
+        {
+            // Choose and attack
+            turnState = TurnState.End;
+        }
+
+        if (Input.GetKeyDown(cancelButton))
+        {
+            // Cancel and go back
+            turnState = TurnState.PickingOption;
+        }
+    }
+
+    void AttackEntity(BattleEntity target, BattleEntity blame, Attack attack)
+    {
+        print($"{blame.name} selected attack: {attack.name} and attacked {target.name}");
+
+        // Perform the attack
+        bool targetDeathState = target.TakeDamage(attack.baseDamage);
+
+        if (targetDeathState) OnEntityDeath(target);
+
+
+        // Clean up UI if player turn
+        if (playerTurn)
+        {
+            // Clean up attack buttons
+            foreach (GameObject button in attackButtons)
+            {
+                Destroy(button);
+            }
+
+            attackButtons.Clear();
+        }
+
+        StartNextTurn();
+    }
+
+    void OnEntityDeath(BattleEntity entity)
+    {
+        print($"{entity.name} has died");
+
+
+        if (playerTurn)
+        {
+            // Killed enemy
+            EnemyParty.Remove(entity.gameObject);
+            Destroy(entity.gameObject);
+
+
+            if (EnemyParty.Count == 0)
+            {
+                battleState = BattleState.Won;
+            }
+        }
+        else
+        {
+            // Killed Player
+            // Player cant attack / downed
+
+            if (PlayerParty.Count == 0)
+            {
+                battleState = BattleState.Lost;
+            }
+        }
+    }
+    #endregion
+
+
+    #region UI
+    void DisplayTargetGraphic()
+    {
+        Transform target = EnemyParty[targetIndex].transform;
+        Vector3 targetPosition = cam.GetComponent<Camera>().WorldToScreenPoint(target.position);
+        targetGraphic.transform.position = targetPosition;
+    }
+
+    void InstantiateAttackButtons()
+    {
+        attackButtonParent.SetActive(true);
+
+        for (int i = 0; i < turnEntity.attacks.Length; i++)
+        {
+            // Store the current attack index to pass to the listener
+            int attackIndex = i;
+
+            GameObject attackButton = Instantiate(attackButtonPrefab, attackButtonParent.transform);
+            attackButtons.Add(attackButton);
+
+            TMP_Text attackText = attackButton.GetComponentInChildren<TMP_Text>();
+            attackText.text = turnEntity.attacks[i].name;
+
+            // Use a lambda to capture the specific attack index
+            attackButton.GetComponent<Button>().onClick.AddListener(() => AttackButton(attackIndex));
+        }
+    }
+
+    public void AttackButton(int attackIndex)
+    {
+        // Find Attack in entity list
+        entityChosenAttack = turnEntity.attacks[attackIndex];
+
+        turnState = TurnState.ChooseTarget;
+    }
+
+    #endregion
+
+
+    #region Helpers
+    void MoveCamToPosition()
+    {
+        //Move camera to first player in queue
+        cam.position = CalculateSpawnPoint(1, 1, playerSpawnRange);
+        cam.position += transform.right * camOffset.x;
+        cam.position += transform.up * camOffset.y;
+        cam.position += transform.forward * camOffset.z;
+
+        cam.LookAt(camGaze);
     }
 
     Vector3 CalculateSpawnPoint(int num, int maxNum, float dist)
@@ -251,7 +455,9 @@ public class BattleController : MonoBehaviour
             }
         }
     }
+    #endregion
 
+    #region Gizmos
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
@@ -278,4 +484,5 @@ public class BattleController : MonoBehaviour
         Gizmos.DrawWireSphere(camPos, 1f);
         Gizmos.DrawWireSphere(camGaze, .3f);
     }
+    #endregion
 }
