f31f5ed7a2dc0a1c038b56d7d79220e39e3ec066
Emanon <155685858+EmanonR@users.noreply.github.com>
2025-03-09 21:53:57 +0100

    Update DialogueTrigger.cs

--- DialogueSystem/DialogueTrigger.cs
+++ DialogueSystem/DialogueTrigger.cs
@@ -2,26 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogueTrigger : MonoBehaviour
+public class DialogueTrigger : Interactable
 {
-    public float InteractRange = 5f;
-    public Vector3 InteractOffset;
-
     public Dialogue dialogueData;
 
-    private void Update()
-    {
-        Transform player = GameManager.instance.player.transform;
-        if (Vector3.Distance(player.position, transform.position + InteractOffset) <= InteractRange)
-        {
-            if (Input.GetKeyDown(GameManager.instance.confirm))
-            {
-                Interact();
-            }
-        }
-    }
-
-    void Interact()
+    public override void Interact()
     {
         if (DialogueManager.instance.inDialogue) return;
 
@@ -32,11 +17,5 @@ public class DialogueTrigger : MonoBehaviour
         }
 
         DialogueManager.instance.StartDialogue(dialogueData);
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position + InteractOffset, InteractRange);
     }
 }
